100da4be03ec2a476c6963f33768ff4b626252d4
mstout60 <mstout@eprospector.com>
2024-04-09 21:57:18 -0500

    Plura website

---
+++ next.config.js
@@ -0,0 +1,35 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'uploadthing.com',
+                pathname: '**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'utfs.io',
+                pathname: '**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'img.clerk.com',
+                pathname: '**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'subdomain',
+                pathname: '**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'files.stripe.com',
+                pathname: '**',
+            },
+        ],
+    },
+    reactStrictMode: false,
+};
+
+module.exports = nextConfig;
