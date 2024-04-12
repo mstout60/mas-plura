ca140b62e8de37399cf56156cb659938a28897af
mstout60 <mstout@eprospector.com>
2024-04-12 09:23:47 -0500

    Setup Team data table

--- src/app/(main)/agency/[agencyId]/team/page.tsx
+++ src/app/(main)/agency/[agencyId]/team/page.tsx
@@ -1,9 +1,72 @@
+import { db } from '@/lib/db'
+import { currentUser } from '@clerk/nextjs'
 import React from 'react'
+import DataTable from './data-table'
+import { Plus } from 'lucide-react'
+import { columns } from './columns'
+import SendInvitation from '@/components/forms/send-invitation'
 
-const Team = () => {
+type Props = {
+  params: { agencyId: string }
+}
+
+const TeamPage = async ({ params }: Props) => {
+  const authUser = await currentUser()
+
+  if (!authUser) return null;
+
+  const teamMembers = await db.user.findMany({
+    where: {
+      Agency: {
+        id: params.agencyId,
+      },
+    },
+    include: {
+      Agency: {
+        include: {
+          SubAccount: true,
+        },
+      },
+      Permissions: {
+        include: {
+          SubAccount: true,
+        },
+      },
+    },
+  });
+
+  if (!teamMembers) return
+
+  const agencyDetails = await db.agency.findUnique({
+    where: {
+      id: params.agencyId,
+    },
+    include: {
+      SubAccount: true,
+    }
+  });
+
+  if (!agencyDetails) return;
+
+
+
   return (
-    <div>Team</div>
+    <DataTable
+      actionButtonText={
+        <>
+          <Plus size={15} />
+          Add
+        </>
+      }
+      modalChildern={<SendInvitation agencyId={agencyDetails.id} />}
+      filterValue='name'
+      columns={columns}
+      data={teamMembers}
+    >
+
+    </DataTable>
+
   )
 }
 
-export default Team
+export default TeamPage
---
+++ src/app/(main)/subaccount/[subaccountId]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const SubaccountPage = (props: Props) => {
+    return (
+        <div>SubaccountPage</div>
+    )
+}
+
+export default SubaccountPage
